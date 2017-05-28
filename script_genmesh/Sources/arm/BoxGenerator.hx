16bcf8e9d606f4a28c7acc0699aee5668503f94f
Lubos Lenco <lubos.lenco@gmail.com>
2017-05-28 14:14:39 +0200

    Build 11

--- script_genmesh/Sources/arm/BoxGenerator.hx
+++ script_genmesh/Sources/arm/BoxGenerator.hx
@@ -10,66 +10,74 @@ import armory.system.Input;
 @:keep
 class BoxGenerator extends armory.Trait {
 
-    var meshData:MeshData;
-    var materials:haxe.ds.Vector<MaterialData>;
+	var meshData:MeshData;
+	var materials:haxe.ds.Vector<MaterialData>;
 
-    public function new() {
-        super();
+	function toF32(ar:Array<Float>):kha.arrays.Float32Array {
+		var vals = new kha.arrays.Float32Array(ar.length);
+		for (i in 0...vals.length) vals[i] = ar[i];
+		return vals;
+	}
 
-        var pos:TVertexArray = {
-            attrib: "position",
-            size: 3,
-            values: [1.0,1.0,-1.0,1.0,-1.0,-1.0,-1.0,-1.0,-1.0,-1.0,1.0,-1.0,1.0,1.0,1.0,-1.0,1.0,1.0,-1.0,-1.0,1.0,1.0,-1.0,1.0,1.0,1.0,-1.0,1.0,1.0,1.0,1.0,-1.0,1.0,1.0,-1.0,-1.0,1.0,-1.0,-1.0,1.0,-1.0,1.0,-1.0,-1.0,1.0,-1.0,-1.0,-1.0,-1.0,-1.0,-1.0,-1.0,-1.0,1.0,-1.0,1.0,1.0,-1.0,1.0,-1.0,1.0,1.0,1.0,1.0,1.0,-1.0,-1.0,1.0,-1.0,-1.0,1.0,1.0]
-        };
+	function toU32(ar:Array<Int>):kha.arrays.Uint32Array {
+		var vals = new kha.arrays.Uint32Array(ar.length);
+		for (i in 0...vals.length) vals[i] = ar[i];
+		return vals;
+	}
 
-        var nor:TVertexArray = {
-            attrib: "normal",
-            size: 3,
-            values: [0.0,0.0,-1.0,0.0,0.0,-1.0,0.0,0.0,-1.0,0.0,0.0,-1.0,0.0,0.0,1.0,0.0,0.0,1.0,0.0,0.0,1.0,0.0,0.0,1.0,1.0,0.0,0.0,1.0,0.0,0.0,1.0,0.0,0.0,1.0,0.0,0.0,0.0,-1.0,-0.0,0.0,-1.0,-0.0,0.0,-1.0,-0.0,0.0,-1.0,-0.0,-1.0,0.0,-0.0,-1.0,0.0,-0.0,-1.0,0.0,-0.0,-1.0,0.0,-0.0,0.0,1.0,0.0,0.0,1.0,0.0,0.0,1.0,0.0,0.0,1.0,0.0]
-        };
+	public function new() {
+		super();
 
-        var indices:TIndexArray = {
-            material: 0,
-            size: 3,
-            values: [0,1,2,0,2,3,4,5,6,4,6,7,8,9,10,8,10,11,12,13,14,12,14,15,16,17,18,16,18,19,20,21,22,20,22,23]
-        };
+		var pos:TVertexArray = {
+			attrib: "pos",
+			size: 3,
+			values: toF32([1.0,1.0,-1.0,1.0,-1.0,-1.0,-1.0,-1.0,-1.0,-1.0,1.0,-1.0,1.0,1.0,1.0,-1.0,1.0,1.0,-1.0,-1.0,1.0,1.0,-1.0,1.0,1.0,1.0,-1.0,1.0,1.0,1.0,1.0,-1.0,1.0,1.0,-1.0,-1.0,1.0,-1.0,-1.0,1.0,-1.0,1.0,-1.0,-1.0,1.0,-1.0,-1.0,-1.0,-1.0,-1.0,-1.0,-1.0,-1.0,1.0,-1.0,1.0,1.0,-1.0,1.0,-1.0,1.0,1.0,1.0,1.0,1.0,-1.0,-1.0,1.0,-1.0,-1.0,1.0,1.0])
+		};
 
-        var rawmesh:TMesh = {
-            primitive: "triangles",
-            vertex_arrays: [pos, nor],
-            index_arrays: [indices]
-        };
+		var nor:TVertexArray = {
+			attrib: "nor",
+			size: 3,
+			values: toF32([0.0,0.0,-1.0,0.0,0.0,-1.0,0.0,0.0,-1.0,0.0,0.0,-1.0,0.0,0.0,1.0,0.0,0.0,1.0,0.0,0.0,1.0,0.0,0.0,1.0,1.0,0.0,0.0,1.0,0.0,0.0,1.0,0.0,0.0,1.0,0.0,0.0,0.0,-1.0,-0.0,0.0,-1.0,-0.0,0.0,-1.0,-0.0,0.0,-1.0,-0.0,-1.0,0.0,-0.0,-1.0,0.0,-0.0,-1.0,0.0,-0.0,-1.0,0.0,-0.0,0.0,1.0,0.0,0.0,1.0,0.0,0.0,1.0,0.0,0.0,1.0,0.0])
+		};
 
-        var rawmeshData:TMeshData = { 
-            name: "BoxMesh",
-            mesh: rawmesh 
-        };
+		var indices:TIndexArray = {
+			material: 0,
+			size: 3,
+			values: toU32([0,1,2,0,2,3,4,5,6,4,6,7,8,9,10,8,10,11,12,13,14,12,14,15,16,17,18,16,18,19,20,21,22,20,22,23])
+		};
 
-        new MeshData(rawmeshData, function(data:MeshData) {
-            // Mesh data parsed
-            meshData = data;
-            meshData.mesh.calculateAABB();
-            
-            // Fetch material from scene data
-            Data.getMaterial("Scene", "Material", function(data:MaterialData) {
-                // Material loaded
-                materials = haxe.ds.Vector.fromData([data]);
-                notifyOnUpdate(update);
-            });
-        });
-    }
+		var rawmeshData:TMeshData = { 
+			name: "BoxMesh",
+			vertex_arrays: [pos, nor],
+			index_arrays: [indices]
+		};
 
-    function update() {
-        // Left mouse button was pressed / display touched / ..
-        if (Input.started) {
-            // Create new object in active scene
-            var object = Scene.active.addMeshObject(meshData, materials);
-            
-            // Just for testing, add rigid body trait
-            var aabb = meshData.mesh.aabb;
-            object.transform.setDimensions(aabb.x, aabb.y, aabb.z);
-            object.transform.set(Math.random() * 8 - 4, Math.random() * 8 - 4, 5);
-            object.addTrait(new armory.trait.internal.RigidBody());
-        }
-    }
+		new MeshData(rawmeshData, function(data:MeshData) {
+			// Mesh data parsed
+			meshData = data;
+			meshData.geom.calculateAABB();
+			
+			// Fetch material from scene data
+			Data.getMaterial("Scene", "Material", function(data:MaterialData) {
+				// Material loaded
+				materials = haxe.ds.Vector.fromData([data]);
+				notifyOnUpdate(update);
+			});
+		});
+	}
+
+	function update() {
+		// Left mouse button was pressed / display touched / ..
+		var mouse = Input.getMouse();
+		if (mouse.started()) {
+			// Create new object in active scene
+			var object = Scene.active.addMeshObject(meshData, materials);
+			
+			// Just for testing, add rigid body trait
+			var aabb = meshData.geom.aabb;
+			object.transform.setDimensions(aabb.x, aabb.y, aabb.z);
+			object.transform.set(Math.random() * 8 - 4, Math.random() * 8 - 4, 5);
+			object.addTrait(new armory.trait.internal.RigidBody());
+		}
+	}
 }
