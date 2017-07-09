73abcdec7a64b69560a0e744aba1f301eb48c98c
Lubos Lenco <lubos.lenco@gmail.com>
2017-07-09 13:02:22 +0200

    Add physics picking example

---
+++ physics_picking/Sources/arm/PickTrait.hx
@@ -0,0 +1,26 @@
+package arm;
+
+import armory.system.Input;
+import armory.trait.internal.PhysicsWorld;
+
+class PickTrait extends armory.Trait {
+
+	public function new() {
+		super();
+
+        notifyOnUpdate(function() {
+            // Check mouse button
+            var mouse = Input.getMouse();
+            if (!mouse.started()) return;
+
+            // Pick object at mouse coords
+            var rb = PhysicsWorld.active.pickClosest(mouse.x, mouse.y);
+            
+            // Check if picked object is our Cube
+            if (rb != null && rb.object.name == 'Cube') {
+                rb.object.transform.translate(0, 0, 1);
+                rb.syncTransform();
+            }
+        });
+	}
+}
