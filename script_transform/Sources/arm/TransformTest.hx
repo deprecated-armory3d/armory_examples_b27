d2f0f65d64ae3f2b07b1968d0d059862c879415f
Lubos Lenco <lubos.lenco@gmail.com>
2017-07-14 18:49:54 +0200

    Transform test example for Build 12

---
+++ script_transform/Sources/arm/TransformTest.hx
@@ -0,0 +1,53 @@
+package arm;
+
+class TransformTest extends armory.Trait {
+    public function new() {
+        super();
+
+        notifyOnInit(function() {
+
+            // Testing 2 cubes, parent cube at (1, 0, 0),
+            // child cube at (5, 0, 0)
+
+            // Parent location
+            trace(object.parent.transform.loc);
+            // (1, 0, 0)
+
+            // Child location
+            trace(object.transform.loc);
+            // (5, 0, 0)
+
+            // Child location in world space
+            trace(object.transform.world.getLoc());
+            // (6, 0, 0)
+
+            // Set child to parent location
+            object.transform.loc.set(0, 0, 0);
+            object.transform.buildMatrix();
+            
+            // Child location
+            trace(object.transform.loc);
+            // (0, 0, 0)
+
+            // Child location in world space
+            trace(object.transform.world.getLoc());
+            // (1, 0, 0)
+
+            // Move parent
+            object.parent.transform.loc.set(0, 3, 0);
+            object.parent.transform.buildMatrix();
+
+            // Child location
+            trace(object.transform.loc);
+            // (0, 0, 0)
+
+            // Child location in world space
+            trace(object.transform.world.getLoc());
+            // (0, 3, 0)
+
+            // Nore: In Blender, you may need to
+            // Clear Parent - Clear Parent Inverse
+            // To get local transform
+        });
+    }
+}
