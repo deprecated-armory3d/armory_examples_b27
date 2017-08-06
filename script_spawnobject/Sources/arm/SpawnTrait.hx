cfbc3c3e60ada6f82bc8c02de1b1e6cd7acafd7f
Lubos Lenco <lubos.lenco@gmail.com>
2017-08-06 19:19:05 +0200

    Spawn object example

---
+++ script_spawnobject/Sources/arm/SpawnTrait.hx
@@ -0,0 +1,23 @@
+package arm;
+
+class SpawnTrait extends armory.Trait {
+	public function new() {
+		super();
+
+		notifyOnInit(function() {
+
+			// Spawn object from scene hierarchy, including children
+			iron.Scene.active.spawnObject("Cube", null, function(object:iron.object.Object) {
+				object.transform.loc.set(0, 4, 0);
+				object.transform.buildMatrix();
+			});
+
+			// Spawn single object from scene hierarchy
+			var spawnChildren = false;
+			iron.Scene.active.spawnObject("Cube", null, function(object:iron.object.Object) {
+				object.transform.loc.set(-4, 0, 0);
+				object.transform.buildMatrix();
+			}, spawnChildren);
+		});
+	}
+}
