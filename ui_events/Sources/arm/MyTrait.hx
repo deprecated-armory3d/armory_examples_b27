34099180722cc97ef4ca8b4d860a70aa391eeca9
Lubos Lenco <lubos.lenco@gmail.com>
2017-08-07 10:45:45 +0200

    Update ui_events

---
+++ ui_events/Sources/arm/MyTrait.hx
@@ -0,0 +1,15 @@
+package arm;
+
+class MyTrait extends armory.Trait {
+	public function new() {
+		super();
+		armory.system.Event.add("button", onEvent);
+	}
+
+	function onEvent() {
+		var loc = object.transform.loc;
+		loc.y += 1;
+		if (loc.y > 4) loc.y -= 8;
+		object.transform.buildMatrix();
+	}
+}
