75f0429057572d833643a6de19877d2b7a5df7ed
Lubos Lenco <lubos.lenco@gmail.com>
2017-08-15 14:08:46 +0200

    Add sound example

---
+++ sound/Bundled/license.md
@@ -0,0 +1,2 @@
+Sounds by Independent.nu licensed under CC0
+https://opengameart.org/content/37-hitspunches
---
+++ sound/Sources/arm/PlaySound.hx
@@ -0,0 +1,21 @@
+package arm;
+
+class PlaySound extends armory.Trait {
+	public function new() {
+		super();
+
+		notifyOnUpdate(function() {
+
+			var mouse = iron.system.Input.getMouse();
+
+			// Mouse clicked
+			if (mouse.started()) {
+
+				// Randomly play one of the three hit sounds
+				iron.data.Data.getSound('hit' + Std.random(3) + '.wav', function(sound:kha.Sound) {
+					iron.system.Audio.play(sound);
+				});
+			}
+		});
+	}
+}
