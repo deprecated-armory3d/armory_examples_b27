faf48583dc85cc3b5ecd4ed5741ad78fc8dea70b
Lubos Lenco <lubos.lenco@gmail.com>
2017-10-15 23:05:40 +0200

    More build 14 examples

---
+++ tilesheet/Sources/arm/AnimControl.hx
@@ -0,0 +1,22 @@
+package arm;
+
+class AnimControl extends armory.Trait {
+
+	var tilesheet:iron.object.Tilesheet;
+
+	public function new() {
+		super();
+
+		notifyOnInit(function() {
+			tilesheet = cast(object, iron.object.MeshObject).tilesheet;
+			iron.system.Tween.timer(Std.random(8), playAnim);
+		});
+	}
+
+	function playAnim() {
+		tilesheet.play("rest", function() {
+			tilesheet.play("idle");
+			iron.system.Tween.timer(Std.random(8), playAnim);
+		});
+	}
+}
