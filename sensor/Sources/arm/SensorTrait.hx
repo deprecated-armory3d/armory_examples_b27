bad94a8496c90b7b597ac55ddf2481da6c0f33fe
Lubos Lenco <lubos.lenco@gmail.com>
2017-10-22 15:56:51 +0200

    Add gyroscope example

---
+++ sensor/Sources/arm/SensorTrait.hx
@@ -0,0 +1,20 @@
+package arm;
+
+import iron.math.Vec4;
+
+class SensorTrait extends armory.Trait {
+	public function new() {
+		super();
+
+		notifyOnInit(function() {
+			var gyro = kha.input.Sensor.get(kha.input.SensorType.Gyroscope);
+			if (gyro != null) gyro.notify(onGyroscope);
+		});
+	}
+
+	function onGyroscope(x:Float, y:Float, z:Float) {
+		object.transform.rotate(Vec4.xAxis(), x / 20);
+		object.transform.rotate(Vec4.yAxis(), y / 20);
+		object.transform.rotate(Vec4.zAxis(), z / 20);
+	}
+}
