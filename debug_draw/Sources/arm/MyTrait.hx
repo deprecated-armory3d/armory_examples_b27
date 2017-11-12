1a5f8025df27d7c65ea576de9d8b4840ea80ac8b
Lubos Lenco <lubos.lenco@gmail.com>
2017-11-12 21:56:27 +0100

    Add debug_draw

---
+++ debug_draw/Sources/arm/MyTrait.hx
@@ -0,0 +1,19 @@
+package arm;
+
+#if arm_debug
+import armory.trait.internal.DebugDraw;
+#end
+
+class MyTrait extends armory.Trait {
+	public function new() {
+		super();
+
+		#if arm_debug
+		DebugDraw.notifyOnRender(function(draw:DebugDraw) {
+			var o = iron.Scene.active.getChild("Suzanne");
+			draw.bounds(o.transform);
+			// draw.line();
+		});
+		#end
+	}
+}
