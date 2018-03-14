a49bbc19205f9ea4c59577dfe3a5d9fc92ee9343
luboslenco <lubos.lenco@gmail.com>
2018-03-14 13:23:21 +0100

    Use the new bake tool

--- material_baked/Sources/arm/ArcBall.hx
+++ material_baked/Sources/arm/ArcBall.hx
@@ -1,25 +1,20 @@
 package arm;
 
-import iron.object.Object;
 import iron.Trait;
 import iron.system.Input;
 import iron.math.Vec4;
-import iron.math.Quat;
 
 class ArcBall extends Trait {
 
 	public function new() {
 		super();
-
 		notifyOnUpdate(update);
 	}
 
 	function update() {
-		if (Input.occupied) return;
-
 		var mouse = Input.getMouse();
 		if (mouse.down()) {
-			object.transform.rotate(new Vec4(0, 0, 1), mouse.movementX / 100);
+			object.transform.rotate(new Vec4(0, 0, 1), -mouse.movementX / 100);
 			object.transform.buildMatrix();
 		}
 	}
