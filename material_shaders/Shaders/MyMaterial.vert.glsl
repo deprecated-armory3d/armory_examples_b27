40a1ce8660f5ffd89d7ce661f6706a2bda272d23
luboslenco <lubos.lenco@gmail.com>
2018-09-19 20:57:50 +0200

    Add param to material_shaders

--- material_shaders/Shaders/MyMaterial.vert.glsl
+++ material_shaders/Shaders/MyMaterial.vert.glsl
@@ -5,11 +5,12 @@ in vec3 nor;
 
 uniform mat4 WVP;
 uniform float time;
+uniform float myParam;
 
 out vec4 p;
 
 void main() {
 	p = vec4(pos, 1.0);
-	p.z += sin((time + pos.x + pos.y) * 2.0) * 0.2;
+	p.z += sin((time + pos.x + pos.y) * 2.0) * 0.2 * myParam;
 	gl_Position = WVP * p;
 }
