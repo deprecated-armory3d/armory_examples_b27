77b6f06aec79277df6418738f35ddb3888c38fe4
luboslenco <lubos.lenco@gmail.com>
2018-09-26 15:26:09 +0200

    Update ui_canvas example

--- ui_canvas/Sources/arm/CanvasTrait.hx
+++ ui_canvas/Sources/arm/CanvasTrait.hx
@@ -11,11 +11,6 @@ class CanvasTrait extends iron.Trait {
 	var canvas:CanvasScript;
 	var clicks = 0;
 
-	function onEvent() {
-		// Set butotn text
-		canvas.getElement("MyButton").text = Std.string(++clicks);
-	}
-
 	public function new() {
 		super();
 
@@ -25,19 +20,35 @@ class CanvasTrait extends iron.Trait {
 
 			// Notify on button click
 			Event.add("buttonEvent", onEvent);
+			Event.add("toggleMenu", onToggleMenu);
+
+			canvas.notifyOnReady(function() {
+				notifyOnUpdate(update);
+			});
 		});
+	}
 
-		notifyOnUpdate(function() {
-			// Canvas may be still being loaded
-			if (!canvas.ready) return;
+	function onEvent() {
+		// Set butotn text
+		canvas.getElement("MyButton").text = Std.string(++clicks);
+	}
 
-			// Set text
-			canvas.getElement("MyText").text = "Hello world";
+	function onToggleMenu() {
+		var shape = canvas.getElement("Shape");
+		shape.visible = !shape.visible;
+	}
 
-			// Move image
-			var img = canvas.getElement("MyImage");
-			var center = App.w() / 2 - img.width / 2;
-			img.x = center + Math.sin(Time.time()) * 100;
-		});
+	function update() {
+		// Canvas may be still being loaded
+		if (!canvas.ready) return;
+
+		// Set text
+		canvas.getElement("MyText").text = "Hello world";
+
+		// Move image
+		var img = canvas.getElement("MyImage");
+		var center = App.w() / 2 - img.width / 2;
+		img.x = center + Math.sin(Time.time()) * 100;
+		img.rotation = canvas.getHandle("Check").selected ? Math.PI / 4 : 0;
 	}
 }
