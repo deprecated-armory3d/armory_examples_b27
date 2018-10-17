7c2f3a34b9a1b6223de00e5ba2066705f9ee5b83
luboslenco <lubos.lenco@gmail.com>
2018-10-17 22:58:34 +0200

    Add load_file example

---
+++ load_file/Sources/arm/LoadFile.hx
@@ -0,0 +1,34 @@
+package arm;
+
+import iron.Scene;
+import iron.data.Data;
+import armory.trait.internal.CanvasScript;
+
+class LoadFile extends iron.Trait {
+	public function new() {
+		super();
+
+		notifyOnInit(function() {
+
+			// Relative or absolute path to file
+			// In this case we load the file placed in the "Bundled/" folder
+			var file = "my_file.json";
+
+			// Load the file asynchronously
+			Data.getBlob(file, function(b:kha.Blob) {
+				// File is now loaded
+				// Get string from loaded bytes
+				var string = b.toString();
+
+				// Get json from string
+				var json = haxe.Json.parse(string);
+
+				// Get canvas trait
+				var canvas = Scene.active.getTrait(CanvasScript);
+
+				// Display "my_text" entry found in the loaded json file
+				canvas.getElement("Text").text = json.my_text;
+			});
+		});
+	}
+}
